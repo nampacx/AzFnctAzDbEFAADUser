ec548d7d84a85f9c39344e393ec18e63a3dfc5a5
cookienowskyj <mkoko@live.de>
2022-10-22 15:03:05 +0200

    added sql with dummy data

---
+++ src/SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.ToDo (
+    Id uniqueidentifier primary key,
+    [order] int null,
+    title nvarchar(200) not null,
+    [url] nvarchar(200) not null,
+    completed bit not null
+);
+
+INSERT INTO [dbo].[ToDo] (Id, [order], title, [url], completed) values (NewID(), 1, 'test', 'https://bing.com', 0)
